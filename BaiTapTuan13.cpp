f65fd065ac098bf4140e209deff178baabec5a41
ngothimaianh <anh.ntm2413974@sis.hust.edu.vn>
2026-06-02 21:54:00 +0700

    btt13ud2

--- BaiTapTuan13.cpp
+++ BaiTapTuan13.cpp
@@ -15,3 +15,21 @@ Node *TaoNut(int x)
     p->right = NULL;
     return p;
 }
+void ThemNode(Node *&T, int x)
+{
+    if (T == NULL)
+    {
+        T = TaoNut(x);
+    }
+    else
+    {
+        if (x < T->dl)
+        {
+            ThemNode(T->left, x);
+        }
+        else
+        {
+            ThemNode(T->right, x);
+        }
+    }
+}
