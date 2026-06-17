ce43c5db9688bd2b7932e73a3e0b5b063c690ab1
ngothimaianh <anh.ntm2413974@sis.hust.edu.vn>
2026-06-17 22:53:16 +0700

    btt15ud7

---
+++ BaiTapTuan15_DanhSachLienKet.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+const int MAX = 20;
+
+string TenTinh[MAX] =
+{
+    "Ha Noi",
+    "Hai Duong",
+    "Hung Yen",
+    "Phu Ly",
+    "Hoa Binh",
+    "Son Tay",
+    "Thai Nguyen",
+    "Bac Ninh",
+    "Bac Giang",
+    "Uong Bi",
+    "Hai Phong"
+};
+int n = 11;
