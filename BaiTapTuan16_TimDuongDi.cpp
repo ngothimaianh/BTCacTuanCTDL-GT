fbd46e3f27414cf5924249a9bf3fd02e236f9420
ngothimaianh <anh.ntm2413974@sis.hust.edu.vn>
2026-06-24 21:20:16 +0700

    btt16ud1

---
+++ BaiTapTuan16_TimDuongDi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+const int MAX = 11;
+string TenTinh[MAX] =
+{
+    "Ha Noi",
+    "Hai Duong",
+    "Hung Yen",
+    "Phu Ly",
+    "Hoa Binh",
+    "Son Tay",
+    "Thai Nguyen",
+    "Bac Ninh",
+    "Bac Giang",
+    "Uong Bi",
+    "Hai Phong"
+};
+
